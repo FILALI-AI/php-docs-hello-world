0cd4eae407357476d45c0400ebb8cca622420385
FILALI-AI <66072253+FILALI-AI@users.noreply.github.com>
2023-04-06 11:13:36 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
 echo "Hello World!";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
